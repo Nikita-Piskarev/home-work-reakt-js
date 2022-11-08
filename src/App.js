33bb2caf05a85b81906579006a478260f09b448e
Nikita Piskarev <npiskarev23@gmail.com>
2022-11-09 00:58:42 +0200

    Fix App and index file commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ProductPen from "./components/ProductPen";
+import ProductPensel from "./components/ProductPensel";
+
+const pensel = {
+  name: "Pensel",
+  description:
+    "Wooden pencil with graphite lead, yellow, with a grout at the end",
+  price: 15,
+  amount: 2,
+};
+
+const pen = {
+  name: "Pen",
+  description:
+    "Blue automatic handle",
+  price: 23,
+  amount: 0,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ProductPensel
+        name={pensel.name}
+        description={pensel.description}
+        price={pensel.price}
+        amount={pensel.amount}
+      />
+      <ProductPen
+        name={pen.name}
+        description={pen.description}
+        price={pen.price}
+        amount={pen.amount}
+      />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,68 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//создал обьект карандаша с описанием
-const product = {
-  name: "pensel",
-  description:
-    "wooden pencil with graphite lead, yellow, with a grout at the end",
-  price: 15,
-  amount: 2,
-};
-// диструризовал его
-const { name, description, price, amount } = product;
-// создал компонент товара через функцию
-function StoreGoods(props) {
-  const { name, value } = props;
 
-  return (
-    <p>
-      {name} {value}
-    </p>
-  );
-}
-
-// создал компонент товара через класс
-class StoreGoods1 extends React.Component {
-  render() {
-    const { name, value } = this.props;
-
-    return (
-      <p>
-        {name} {value}
-      </p>
-    );
-  }
-}
-
-// вывел на экран сразу 2 компонента функции и классовб установиил заглушки на пропсы, а так же указал о наличии товара на складе;
-root.render(
-  <div>
-    <StoreGoods1 name="Name - " value={name ? name : "not specified"} />
-
-    <StoreGoods1
-      name="Description - "
-      value={description ? description : "not specified"}
-    />
-
-    <StoreGoods1 name="Price - " value={price ? price : "not specified"} />
-
-    <StoreGoods1 name="Amount - " value={amount ? "in stock" : "not stock"} />
-
-    <StoreGoods name="Name - " value={name ? name : "not specified"} />
-
-    <StoreGoods
-      name="Description - "
-      value={description ? description : "not specified"}
-    />
-
-    <StoreGoods name="Price - " value={price ? price : "not specified"} />
-
-    <StoreGoods name="Amount - " value={amount ? "in stock" : "not stock"} />
-  </div>
-);
-// не знаю пока куда отдельно можно положить файлы по этому сделал все сдесь
+root.render(<App/>);
 
 reportWebVitals();
